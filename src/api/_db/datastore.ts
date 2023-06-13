057d330984727fbc3386338135aa9ee673ed6dd9
Savas Vedova <savas@stormkit.io>
2023-06-13 15:38:14 +0200

    feat: add support for stormkit datastore

---
+++ src/api/_db/datastore.ts
@@ -0,0 +1,25 @@
+import type { Store } from "./store.d";
+import ds from "@stormkit/ds";
+
+const store: Store = {
+  templates: {
+    async list() {
+      return ds.fetch<Template>("templates");
+    },
+
+    async byId(recordId: string) {
+      return ds.fetchOne<Template>("templates", { recordId });
+    },
+
+    async store(template: Template) {
+      const result = await ds.store("templates", template, {
+        unique: ["name"],
+      });
+
+      template.recordId = result.recordIds?.[0];
+      return template;
+    },
+  },
+};
+
+export default store;
--- src/api/_db/index.ts
+++ src/api/_db/index.ts
@@ -3,7 +3,8 @@ import type { Store } from "./store.d";
 export default async function (): Promise<Store> {
   // If it's Stormkit, return the datastore
   if (process.env.STORMKIT === "true") {
-    // TODO
+    const { default: datastore } = await import("./datastore");
+    return datastore;
   }
 
   // Otherwise fallback to sqlite3
